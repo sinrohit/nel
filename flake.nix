af4e3658aaae85e280b9385cd29a1846b4d8c162
Rohit Singh <rsrohitsingh682@gmail.com>
2025-03-02 05:27:40 +0530

    flake-parts init

--- flake.nix
+++ flake.nix
@@ -1,124 +1,113 @@
-# flake.nix
 {
   description = "Custom environment loader with NixOS-like module system";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = nixpkgs.lib;
+  outputs = { self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        # Load the user's configuration
-        userConfig = import ./config.nix;
 
-        # Process the configuration in a NixOS-like way
-        config = lib.evalModules {
-          modules = [
-            ./modules/base.nix # Contains the module structure definition
-            ./modules/activation.nix # NixOS-like activation system
-            ./modules/programs/zsh.nix # ZSH-specific module
-            ./modules/programs/vim.nix # Vim-specific module
-            # Add more program-specific modules here
-            userConfig # User's configuration
-          ];
-          # Provide access to nixpkgs to the modules
-          specialArgs = { inherit pkgs lib; };
-        };
+      # Import modules within perSystem scope
+      imports = [
+        ./modules/default.nix
+      ];
 
-        # Extract the evaluated config
-        evaluatedConfig = config.config;
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
 
-        # Extract the environment packages from the evaluated config
-        environmentPackages = evaluatedConfig.environment.systemPackages;
+        imports = [
+          ./config.nix
 
-        # Get sorted activation scripts
-        activationScripts =
+        ];
+
+        # Define the outputs
+        apps =
           let
-            toposort = import ./modules/toposort.nix { inherit lib; };
-            allScripts =
-              builtins.attrValues evaluatedConfig.system.activationScripts
-              ++ builtins.attrValues evaluatedConfig.home.activationScripts;
-            getDeps = script:
-              map
-                (dep:
-                  if evaluatedConfig.system.activationScripts ? ${dep} then
-                    evaluatedConfig.system.activationScripts.${dep}
-                  else if evaluatedConfig.home.activationScripts ? ${dep} then
-                    evaluatedConfig.home.activationScripts.${dep}
-                  else
-                    throw "Activation script '${dep}' not found"
-                )
-                script.deps;
+            environmentPackages = config.environment.systemPackages;
+
+            activationScripts =
+              let
+                toposort = import ./modules/toposort.nix { inherit lib; };
+                allScripts =
+                  builtins.attrValues config.system.activationScripts
+                  ++ builtins.attrValues config.home.activationScripts;
+                getDeps = script:
+                  map
+                    (dep:
+                      if config.system.activationScripts ? ${dep} then
+                        config.system.activationScripts.${dep}
+                      else if config.home.activationScripts ? ${dep} then
+                        config.home.activationScripts.${dep}
+                      else
+                        throw "Activation script '${dep}' not found"
+                    )
+                    script.deps;
+              in
+              toposort.toposort getDeps allScripts;
+
+            envScript = pkgs.writeShellScriptBin "load-env" ''
+              # Create runtime directory structure
+              TMP_DIR=$(mktemp -d -t nix-env-loader-XXXXXXX)
+              trap 'rm -rf "$TMP_DIR"' EXIT
+            
+              # Run all activation scripts in dependency order
+              ${lib.concatMapStrings (script: ''
+                echo "Running activation script: ${script.name}"
+                ${script.text}
+              '') activationScripts}
+            
+              # Set up environment variables
+              export PATH=${lib.makeBinPath environmentPackages}:$PATH
+              ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: 
+                if builtins.isList value 
+                then "export ${name}=${lib.concatStringsSep ":" value}"
+                else "export ${name}=${value}"
+              ) config.environment.variables)}
+            
+              # Set XDG_CONFIG_HOME to point to the temp directory
+              export XDG_CONFIG_HOME="$TMP_DIR/.config"
+            
+              # Print available packages
+              echo "Environment loaded with these packages:"
+              echo "${lib.concatMapStringsSep "\n" (pkg: "- ${pkg.name}") environmentPackages}"
+            
+              # If ZSH is enabled, use it as the shell
+              ${lib.optionalString config.programs.zsh.enable ''
+                # For zsh, we need to set ZDOTDIR to our temp directory
+                if [ $# -gt 0 ]; then
+                  ZDOTDIR="$TMP_DIR" exec ${pkgs.zsh}/bin/zsh -c "exec \"\$@\""
+                else
+                  ZDOTDIR="$TMP_DIR" exec ${pkgs.zsh}/bin/zsh
+                fi
+              ''}
+            
+              # Otherwise use bash with aliases
+              ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: 
+                "alias ${name}='${value}'"
+              ) config.environment.shellAliases)}
+            
+              if [ $# -gt 0 ]; then
+                exec "$@"
+              else
+                exec ${pkgs.bash}/bin/bash
+              fi
+            '';
           in
-          toposort.toposort getDeps allScripts;
+          {
+            default = {
+              type = "app";
+              program = "${envScript}/bin/load-env";
+            };
+          };
 
-        # Create a shell script that sets up the environment
-        envScript = pkgs.writeShellScriptBin "load-env" ''
-          # Create runtime directory structure
-          TMP_DIR=$(mktemp -d -t nix-env-loader-XXXXXXX)
-          trap 'rm -rf "$TMP_DIR"' EXIT
-          
-          # Run all activation scripts in dependency order
-          ${lib.concatMapStrings (script: ''
-            echo "Running activation script: ${script.name}"
-            ${script.text}
-          '') activationScripts}
-          
-          # Set up environment variables
-          export PATH=${lib.makeBinPath environmentPackages}:$PATH
-          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: 
-            if builtins.isList value 
-            then "export ${name}=${lib.concatStringsSep ":" value}"
-            else "export ${name}=${value}"
-          ) evaluatedConfig.environment.variables)}
-          
-          # Set XDG_CONFIG_HOME to point to the temp directory
-          export XDG_CONFIG_HOME="$TMP_DIR/.config"
-          
-          # Print available packages
-          echo "Environment loaded with these packages:"
-          echo "${lib.concatMapStringsSep "\n" (pkg: "- ${pkg.name}") environmentPackages}"
-          
-          # If ZSH is enabled, use it as the shell
-          ${lib.optionalString evaluatedConfig.programs.zsh.enable ''
-            # For zsh, we need to set ZDOTDIR to our temp directory
-            if [ $# -gt 0 ]; then
-              ZDOTDIR="$TMP_DIR" exec ${pkgs.zsh}/bin/zsh -c "exec \"\$@\""
-            else
-              ZDOTDIR="$TMP_DIR" exec ${pkgs.zsh}/bin/zsh
-            fi
-          ''}
-          
-          # Otherwise use bash with aliases
-          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: 
-            "alias ${name}='${value}'"
-          ) evaluatedConfig.environment.shellAliases)}
-          
-          if [ $# -gt 0 ]; then
-            exec "$@"
-          else
-            exec ${pkgs.bash}/bin/bash
-          fi
-        '';
-      in
-      {
-        # The app that can be run with `nix run`
-        apps.default = {
-          type = "app";
-          program = "${envScript}/bin/load-env";
-        };
-
-        # For development
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nixpkgs-fmt
           ];
         };
-      }
-    );
+      };
+    };
 }
